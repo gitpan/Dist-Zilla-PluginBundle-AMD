9d91bbd88b61e71429d6f1b6ec14737c930b5b04
AMD OSRC (AMD Operating System Research Center) <tapper@tapper-testing.org>
2012-05-25 13:28:09 +0000

    Import of AMD/Dist-Zilla-PluginBundle-AMD-0.006 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-PluginBundle-AMD
    gitpan-cpan-version:      0.006
    gitpan-cpan-path:         AMD/Dist-Zilla-PluginBundle-AMD-0.006.tar.gz
    gitpan-cpan-author:       AMD
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/PluginBundle/AMD.pm
+++ lib/Dist/Zilla/PluginBundle/AMD.pm
@@ -3,7 +3,7 @@ BEGIN {
   $Dist::Zilla::PluginBundle::AMD::AUTHORITY = 'cpan:AMD';
 }
 {
-  $Dist::Zilla::PluginBundle::AMD::VERSION = '4.0.3';
+  $Dist::Zilla::PluginBundle::AMD::VERSION = '0.006';
 }
 # ABSTRACT: Build your distributions like AMD does
 
